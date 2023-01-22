8ff46d008a82aa2f7c0b3daafdbb1939ca9bedcb
Rudi <rudolf.buettner@gmail.com>
2023-01-22 20:35:41 +0000

    9

--- Reim.js
+++ Reim.js
@@ -2,19 +2,26 @@ const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 const app = express();
 
+// Use the API key from the environment variable
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
   
 app.get("/", async (req, res) => {
+  try {
     const response = await openai.createCompletion({
         model: "text-davinci-002",
         prompt: "Was reimt sich auf Schleim?",
         max_tokens: 10,
         });
     const rhyme = response.choices[0].text;
-    res.send('The rhyme for "Schleim" is:');
-   }
-);
+    console.log(rhyme);
+    res.send(`
+        <p>The rhyme for "Summe" is: ${rhyme}</p>
+    `);
+  } catch (err) {
+    console.log(err);
+  }
+});
 app.listen(process.env.PORT||3000)
